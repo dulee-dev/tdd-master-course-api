83140963175662d5e98633ea003bcdce991f3e72
dulee <dulee.dev@gmail.com>
2025-01-24 13:43:09 +0900

    ..

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # tdd-master-course-api
 
+1. fixture적용
+
+/src/db가 db 역할을 합니다 contents.ts, users.ts에
+
+사용중인 fixture를 넣어주세요
+
+2. npx server 활용법
+
 dist에 생성된 파일에 #!/usr/bin/env node
-추가해줘야 npx실행가능
+추가해줘야 npx실행가능(npx server)
+
+build 후 활용해주세요
--- src/db/users.ts
+++ src/db/users.ts
@@ -1,8 +1,14 @@
 import { User } from '../schema/user.entity.js';
 
 export const userTable: User[] = [
-  { id: '437558cf-9e5a-413c-81e9-e06d1ec842d9', nickname: 'dulee' },
-  { id: 'cbea24da-143e-4add-9d99-eb84897bcaa2', nickname: 'Denis' },
-  { id: 'd8cf3510-041c-4f9d-9dae-f3e324c28ffe', nickname: 'Carmine' },
-  { id: '2f7b7425-f24a-4335-9e29-318c852049a7', nickname: 'Geraldine' },
+  {
+    id: '39bf588d-871d-4c51-8d60-1b5df88c1dbc',
+    nickname: 'dulee',
+    imgUrl: '/window.svg',
+  },
+  {
+    id: '2f11b583-61d4-4106-bc66-03ebd8a3dbf5',
+    nickname: 'Anabelle',
+    imgUrl: '/globe.svg',
+  },
 ];
